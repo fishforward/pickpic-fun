e4839d9319ad2e8e1678ba457b8af514fc261e09
fish <yy_8789@126.com>
2011-08-07 22:21:37 +0800

    commit-201108079-3

--- app/models/photo.rb
+++
@@ -1,6 +0,0 @@
-class Photo < ActiveRecord::Base
-  # validates_format_of :image, :with=>/^.*(.jpg|.JPG|.gif|.GIF)$/, :message => "Only jpg/gif Supported!"
-  file_column :image, :magick => {       
-          :versions => { "thumb" => "306x306!" }      
-        }      
-end
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,4 +1,3 @@
-encoding: utf-8
 
 class Pic < ActiveRecord::Base
   # attr_accessible :title, :image_url
